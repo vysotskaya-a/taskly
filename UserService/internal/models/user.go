38f10660af74df40a7dae1bfa9fcf5204a000d6d
Anna <2006apv@gmail.com>
2024-11-30 23:08:37 +0300

    User API 2

--- UserService/internal/models/user.go
+++ UserService/internal/models/user.go
@@ -1,1 +1,8 @@
 package models
+
+type User struct {
+	ID       string `json:"id" db:"id_uuid"`
+	Email    string `json:"email" db:"email"`
+	Password string `json:"password,omitempty" db:"password_hash"`
+	Grade    string `json:"grade" db:"grade"`
+}
